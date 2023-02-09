2c358ddbf966a96b0c7b93c9aa1e9a31ef3a3f75
xirtoth <henry.heino11@gmail.com>
2023-02-09 15:44:06 +0200

    Lisättiin vs projekti
    vs projekti ja tyhjä GameController luokka

---
+++ GameController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace School_Project
+{
+    public class GameController
+    {
+        public static GameController Instance { get; set; }
+        public GameController()
+        {
+
+        }
+
+        public void Init()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace School_Project
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            GameController gc = GameController.Instance;
+            gc.Init();
+        }
+    }
+}
