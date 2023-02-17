6a8fbd204590535dfc980cf0da29f699487da67a
Joon <sarin.joonas@live.fi>
2023-02-17 13:41:15 +0200

    PlayerMovement

--- PlayerMovement.cs
+++ PlayerMovement.cs
@@ -1,1 +1,79 @@
-﻿
+﻿using System;
+
+namespace School_Project
+{
+    public class PlayerMovement
+    {
+        private Map map;
+        private int x;
+        private int y;
+
+        public PlayerMovement(Map map, int startX, int startY)
+        {
+            this.map = map;
+            this.x = startX;
+            this.y = startY;
+            this.map.SetPlayerPosition(x, y);
+        }
+
+        public void MoveUp()
+        {
+            int newX = x;
+            int newY = y - 1;
+
+            if (map.IsPositionValid(newX, newY))
+            {
+                map.ClearPlayerPosition(x, y);
+                x = newX;
+                y = newY;
+                map.SetPlayerPosition(x, y);
+                map.Draw();
+            }
+        }
+
+        public void MoveDown()
+        {
+            int newX = x;
+            int newY = y + 1;
+
+            if (map.IsPositionValid(newX, newY))
+            {
+                map.ClearPlayerPosition(x, y);
+                x = newX;
+                y = newY;
+                map.SetPlayerPosition(x, y);
+                map.Draw();
+            }
+        }
+
+        public void MoveLeft()
+        {
+            int newX = x - 1;
+            int newY = y;
+
+            if (map.IsPositionValid(newX, newY))
+            {
+                map.ClearPlayerPosition(x, y);
+                x = newX;
+                y = newY;
+                map.SetPlayerPosition(x, y);
+                map.Draw();
+            }
+        }
+
+        public void MoveRight()
+        {
+            int newX = x + 1;
+            int newY = y;
+
+            if (map.IsPositionValid(newX, newY))
+            {
+                map.ClearPlayerPosition(x, y);
+                x = newX;
+                y = newY;
+                map.SetPlayerPosition(x, y);
+                map.Draw();
+            }
+        }
+    }
+}
--- Screen.cs
+++ Screen.cs
@@ -64,11 +64,40 @@ namespace School_Project
             // Set the player position on the map
             gameMap.SetPlayerPosition(5, 5);
 
+            // Create a new instance of the PlayerMovement class
+            PlayerMovement playerMovement = new PlayerMovement(gameMap, 5, 5);
+
             // Draw the game board
             gameMap.Draw();
 
             // Print the player on the screen
             PrintPlayer();
+
+            // Keep moving the player until the user presses the Esc key
+            while (true)
+            {
+                // Get the key pressed by the user
+                ConsoleKeyInfo keyInfo = Console.ReadKey(true);
+
+                // Move the player in the corresponding direction
+                switch (keyInfo.Key)
+                {
+                    case ConsoleKey.UpArrow:
+                        playerMovement.MoveUp();
+                        break;
+                    case ConsoleKey.DownArrow:
+                        playerMovement.MoveDown();
+                        break;
+                    case ConsoleKey.LeftArrow:
+                        playerMovement.MoveLeft();
+                        break;
+                    case ConsoleKey.RightArrow:
+                        playerMovement.MoveRight();
+                        break;
+                    case ConsoleKey.Escape:
+                        return;
+                }
+            }
 
             // Update the player position
             gameMap.ClearPlayerPosition(5, 5);
