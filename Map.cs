f6f82627c94b459261e978d06e038d0366a1fe32
xirtoth <henry.heino11@gmail.com>
2023-03-19 11:40:54 +0200

    Update Map.cs

--- Map.cs
+++ Map.cs
@@ -36,68 +36,14 @@ namespace School_Project
         }
 
 
-        // Update the map with player's current position
-        public void SetPlayerPosition(int x, int y)
-        {
-            Mapping[y, x] = '@';
-        }
-        // Clear player's previous position on the map
-        public void ClearPlayerPosition(int x, int y)
-        {
-            Mapping[y, x] = '.';
-        }
-
-        // Check if a given position is within the bounds of the game board
+    
+        // Check if a given position is within the bounds of the game board and if there is wall at position
         public bool IsPositionValid(int x, int y)
         {
             
             return x >= 0 && x < Width && y >= 0 && y < Height && Mapping[x, y] != '#';
         }
-
-        // Draw the game board to the console
-        public void Draw()
-        {
-            Console.Clear();
-            for (int y = 0; y < Height; y++)
-            {
-                for (int x = 0; x < Width; x++)
-                {
-                    if (Mapping[y, x] == '@')
-                    {
-                        // Set player's character to green
-                        Console.ForegroundColor = ConsoleColor.Green;
-                    }
-                    Console.Write(Mapping[y, x]);
-                    Console.ResetColor();
-                }
-                Console.WriteLine();
-            }
-        }
-
-        // Draw the game board to the console, including edges of the board
-        public void DrawMap()
-        {
-            Console.Clear();
-            for (int y = 0; y < Mapping.GetLength(0); y++)
-            {
-                for (int x = 0; x < Mapping.GetLength(1); x++)
-                {
-                    if (Mapping[y, x] == '@')
-                    {
-                        // Set player's character to green
-                        Console.ForegroundColor = ConsoleColor.Green;
-                    }
-                    else if (Mapping[y, x] == '#')
-                    {
-                        // Set edges of the board to gray
-                        Console.ForegroundColor = ConsoleColor.Gray;
-                    }
-                    Console.Write(Mapping[y, x]);
-                    Console.ResetColor();
-                }
-                Console.WriteLine();
-            }
-        }
+  
 
         // Update the game board with a new 2D char array
         public void UpdateMap(char[,] updatedBoard)
