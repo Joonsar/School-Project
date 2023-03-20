157d25c362d552243e8fb7dc538bbb2e5e0d7589
xirtoth <henry.heino11@gmail.com>
2023-03-20 09:04:51 +0200

    Update Map.cs
    korjattu bugi mapin tulostamisesta

--- Map.cs
+++ Map.cs
@@ -21,7 +21,7 @@ namespace School_Project
             {
                 for (int y = 0; y < height; y++)
                 {
-                    if (x == 0 || x == width || y == 0 || y == height)
+                    if (x == 0 || x == width -1 || y == 0 || y == height 1)
                     {
                         // Place "#" at edges of the board
                         Mapping[x, y] = '#';
