0cd4a285b5414b2f1e31afa6a0d748eb2cc5d158
xirtoth <henry.heino11@gmail.com>
2023-04-03 22:15:33 +0300

    Tein nopeesti MapObject luokan

---
+++ MapObject.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace School_Project
+{
+    public class MapObject
+    {
+        public char Mark { get; set; }
+        public string Description { get; set; }
+
+        public bool CanWalkThrough { get; set; }
+        public ConsoleColor Color;
+
+        public MapObject(char mark, string description, bool canWalkThrhough)
+        {
+            Mark = mark;
+            Description = description;
+            CanWalkThrough = canWalkThrhough;
+        }
+
+        public MapObject(char mark, string description, bool canWalkThrhough, ConsoleColor color)
+        {
+            Mark = mark;
+            Description = description;
+            CanWalkThrough = canWalkThrhough;
+            Color = color;
+        }
+    }
+}
