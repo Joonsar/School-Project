07b60481d1304c2521bb55ebdccead4a8f78c116
xirtoth <henry.heino11@gmail.com>
2023-04-17 09:59:56 +0300

    lisättiin sqlite nugetti ja item classin pohja

---
+++ Item.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace School_Project
+{
+    public class Item : Entity
+    {
+        
+
+        public Item(string name, string description, Position pos, char mark, ConsoleColor color) : base(name, description, pos, mark, color)
+        {
+
+        }
+        
+    }
+}
