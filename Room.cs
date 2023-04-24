50456b7a5010da548f6ef347105066a270e16638
Joonas Sarin <sarin.joonas@live.fi>
2023-04-24 09:14:20 +0300

    Room update

--- Room.cs
+++ Room.cs
@@ -23,8 +23,18 @@ namespace School_Project
             Random random = new Random();
             int width = random.Next(5, 14); // Random width between
             int height = random.Next(5, 14); // Random height between
-            int x = random.Next(1, gc.Width - width - 1); // Random x position within the game board
-            int y = random.Next(1, gc.Height - height - 1); // Random y position within the game board
+            int corridorWidth = 1; // Width of the corridor
+            int x = random.Next(1, gc.Width - roomWidth - corridorWidth - 1); // Random x position within the game board
+            int y = random.Next(1, gc.Height - roomHeight - corridorWidth - 1); // Random y position within the game board
+
+            // Add an empty row and column to the room dimensions
+            int width = roomWidth + corridorWidth;
+            int height = roomHeight + corridorWidth;
+
+            // Shift the room position to account for the added row and column
+            x += corridorWidth;
+            y += corridorWidth;
+
             Rect = new Rectangle(x, y, width, height);
 
 
