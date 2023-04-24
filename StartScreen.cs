5c5eeb587f31673f4af575e3643164701829a7fd
Joonas Sarin <sarin.joonas@live.fi>
2023-04-24 10:30:39 +0300

    StartScreen

--- StartScreen.cs
+++ StartScreen.cs
@@ -37,21 +37,23 @@ namespace School_Project
                 }
                 if (this.command == "2")
                 {
+
+
                 }
                 if (this.command == "3")
                 {
                     Console.Clear();
-                    Console.WriteLine(" ");
-                    Console.WriteLine("- Liikuta pelaajaa nuoli- tai numpad näppäimillä.");
                     Console.WriteLine(" ");
                     Console.WriteLine("Merkit:");
                     Console.WriteLine(" # - Seinä               + - Ovi");
                     Console.WriteLine(" ! - Itemi               Kirjaimet - Vastustajia");
                     Console.WriteLine(" < - Rappuset ylös       > - Rappuset alas");
                     Console.WriteLine(" ");
-                    Console.WriteLine(" - Paina Välilyöntiä tutkiaksesi maailmaa.");
+                    Console.WriteLine("- Paina Välilyöntiä tutkiaksesi maailmaa.");
                     Console.WriteLine(" ");
-                    Console.WriteLine(" - Paina Esc lopettaaksesi pelin");
+                    Console.WriteLine("- Liikuta pelaajaa nuoli- tai numpad näppäimillä.");
+                    Console.WriteLine(" ");
+                    Console.WriteLine("- Paina Esc lopettaaksesi pelin");
                     System.Threading.Thread.Sleep(3000);
                     this.PrintInfo();
                 }
