befdf93eb2eb0f81abdee17b56994fc5f4660846
xirtoth <henry.heino11@gmail.com>
2023-04-27 21:14:46 +0300

    readonly maxplayernamelength

--- StartScreen.cs
+++ StartScreen.cs
@@ -10,7 +10,7 @@ namespace School_Project
         private static System.Media.SoundPlayer player = new System.Media.SoundPlayer();
         private readonly string IntroText = "       _                                 _             _             \r\n      | |                               | |           (_)            \r\n      | |_   _  ___  _ __   ___  _ __   | |_ __ _ _ __ _ _ __   __ _ \r\n  _   | | | | |/ _ \\| '_ \\ / _ \\| '_ \\  | __/ _` | '__| | '_ \\ / _` |\r\n | |__| | |_| | (_) | |_) | (_) | | | | | || (_| | |  | | | | | (_| |\r\n  \\____/ \\__,_|\\___/| .__/ \\___/|_| |_|  \\__\\__,_|_|  |_|_| |_|\\__,_|\r\n                    | |                                              \r\n                    |_|      ";
         private readonly ConsoleColor[] Colors = { ConsoleColor.Red, ConsoleColor.Green, ConsoleColor.Yellow, ConsoleColor.Blue, ConsoleColor.Magenta, ConsoleColor.Cyan };
-        private int maxPlayerNameLength = 15;
+        private readonly int maxPlayerNameLength = 15;
         public string PlayerName { get; set; }
 
         public StartScreen()
