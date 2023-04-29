3513a9fa2cbdf486e33d29a8326c6c17d94b01be
ilkkahanninen1 <ilkka.hanninen@edu.sasky.fi>
2023-04-29 14:24:31 +0300

    Update Enemies.cs

--- Enemies.cs
+++ Enemies.cs
@@ -20,7 +20,19 @@ namespace School_Project
             "Kovaa juovien porukassa, mutta ei itse ole kova juomaan.","Koko vkl putkeen!","Sixpäkki duunin jälkee heti pöytää!",
             "Juo pelkkää olutta ja korvaa kaiken sanomansa PERKELE huudolla","Juo salaa.. hyi!","melkonen juoppo,Välillä huiliiki",
             "Juo käänit ja aiheuttaa pahennusta kylillä","Välillä heilahtaa viikko kaks..","Katkolta kotiin alkon kautta","Kaikki tauluu mitä löytyy",
-            "Virtsan tuoksu hiipii nenää jo kaukaa","Täyspäivänen duuni pysyy tönössä..","Nesteeltä lasolia vaikka pikkasen iho kellertääki"
+            "Virtsan tuoksu hiipii nenää jo kaukaa","Täyspäivänen duuni pysyy tönössä..","Nesteeltä lasolia vaikka pikkasen iho kellertääki",
+            "Tuntuu olevan aina juhlissa mukana, missä tahansa.","Lempparijuoma on tequila, mutta kelpaa mikä tahansa poltteleva.","Häröilee joka kerta ja aiheuttaa aina draamaa.",
+            "Onnistuu aina juomaan itsensä sammaksi asti.","Ei osaa pitää juomistaan erillään työpäivän jälkeen.","On ihan kiva tyyppi muulloin, mutta juodessaan muuttuu täysin.",
+            "Saa päähänsä huumorin vain itse humalassa ollessaan.","Pysyy tiukassa dieetissä viikon ajan, kunnes lähtee viikonlopuksi juhlimaan kaiken takaisin.",
+            "Tuntuu keskittyvän pelkästään juomansa määrään, eikä juuri muuhun.","Tykkää kokeilla kaikkea uutta ja trendikästä.",
+            "Juo pitkään, mutta pysyy kunnossa ilman sammumisia.","Tietää, mitä haluaa, juomisen suhteen ja muutenkin.",
+            "Juo aina liikaa, mutta osaa ainakin nauraa sille itsekin.","Väittää olevansa elämää suurempi, mutta humalassa tulee syvät suvantovaiheet.",
+            "Tykkää juoda yksin kotonaan, mutta sitten lähtee pahasti hukkaan jossain baarissa.","Vaarallisen korkeakierroksinen juoma-asia, joka heittelee mieltä ja ruumista.",
+            "Pitää vähän kaikista juomista, mutta sekaan mahtuu monesti myös aineita.","Juot kaikesta huolimatta ilostuneena ja hyväntuulisena.",
+            "Juot juuri sen verran, että alkaa puhua itsestään kolmannessa persoonassa.","Juot vain kotona tai luonnon helmassa, jolloin kukaan ei näe hänen käytöstään.",
+            "Ei tee koskaan hommia ennen kuin juo ensin aamukahvin ja paloviinan.","On elämänsä kunnossa vain juodessaan, muuten vetää koko ajan ylihilseen.",
+            "Juot kännissä vain yhden tunnin karuselli-ajelulaitteessa.","On pitänyt paussia juomisesta jo viisi vuotta, mutta silloin tällöin kostautuu entisellä kuumalla asialla."
+
         };
 
         private string[] enemyNames = new string[]
@@ -29,7 +41,17 @@ namespace School_Project
             "Iltaisin Ilkka","Kaarle Kustaa","Kaisa-Maija","Hilkka Hiljainen","Vihainen Väinö","Tuhma-Tuulia","Puliveivi-Petteri","Korjari-Kalle","Kuura-Kaisa","Kierosilmä-Kalle",
             "Kukko-Kustaa","Vihreä-Vilho","Röyhkeä-Riku","Sähäkkä-Sari","Sini-Simo","Karski-Kalle","Leijuva-Liisa","Pimeyden Pekka","Suksi-Sakari","Timanttinen Tiina","Ukko-Pekka",
             "Vilkas-Ville","Juoppo Jaska","Piilojuoppo Pekka","Rapajuoppu Reino","Pelkkä Keijo","Ex nyrkkeilijä puistosta","Semi pro","Lasse lähtilapasesta","Ihan vaan Seppo","Taiteilija Thomas",
-            "Pelle Pöhnä","Märkäkorva Marko","Pimeyden Reino","Viinapiru Väinö","Pro","Puiston Jaska","Ihan vaan ammattilainen","Delirium topi","Kadun mies","Puiston asukki"
+            "Pelle Pöhnä","Märkäkorva Marko","Pimeyden Reino","Viinapiru Väinö","Pro","Puiston Jaska","Ihan vaan ammattilainen","Delirium topi","Kadun mies","Puiston asukki",
+            "Huligaani Heikki", "Äkäinen Aki", "Väkivaltainen Valtteri", "Kähärä-Kalle", "Vino-Ville", "Kauhea Kusti", "Rotsi-Rami", "Jyrä-Janne", "Hinaaja Henri", "Myrsky-Matti",
+            "Riehakas Rami", "Köriläs Kauko", "Sähikäinen Sari", "Muukalainen Martti", "Huutava Heimo", "Roistava Raimo", "Viikinki-Veikko", "Sokeus-Satu", "Jallumainen Jarkko",
+            "Räyhäävä Raisa", "Rehti-Reijo", "Kuolinki-Kari", "Mutkikas Matti", "Nyrkkeilevä Nanna", "Mustahuuli Mika", "Anarkista Antti", "Pimpottaja Päivi", "Karjuva Kari", "Pöljä Pasi",
+            "Vieteri-Ville","Pikku-Päivi","Höpinä-Helena","Hämy-Hannu","Ruoste-Raimo","Näätä-Niina","Rauni Rämäpää","Mörkö-Matti","Loistava Lassi","Vauhti-Valtteri","Onnen-Onni",
+            "Hän,Oikea Hän","Lupaus-Laura","Heinäsirkka-Helmi","Sisu-Sampsa","Pajunkissapoika-Pekka","Jippo-Jarmo","Siivetön-Sampo","Juuri-Juha","Särkänsilmä-Sari","Salamakypärä-Simo",
+            "Loisto-Liisa","Etsivä-Eemeli","Räjähtävä-Roope","Pikkupomo-Pekka","Herrasväki-Helena","Pölyinen-Pekka","Lohikäärme-Leena","Divari-David","Tohina-Timo","Riemurinnassa-Riikka",
+            "Jännityksen-Jaska","Tämä-Timo","Piru-Pete","Tunturi-Taru","Kyhmyinen-Kaija","Nenäliina-Niilo","Härski-Hilma","Riemukas-Risto","Jäykistävä-Jani","Tihkuisa-Tuomas",
+            "Viheltelijä-Veeti","Käpylän-Kirsi","Uhuva-Urho","Sopuli-Saara","Punaposki-Pertti","Seksikäs-Salla","Mummotunneli-Mikko","Puhurin-Pirkka","Robotti-Roope","Kevätpörriäinen-Kaisa",
+            "Riemukupla-Roope","Nyyhky-Niina","Mykerö-Maija","Sutjakka-Sakari","Uutisankka-Ulla","Kilja-Kylli","Silkkisukka-Sakke","Tukkapölly-Tuomas","Romanttinen Raimo","Verbaalinen-Ville",
+            "Hippiveli-Hannu","Jatkuva-Juha","Kohiseva-Kuisma","Hirviö-Hanna","Ryminä-Risto","Kirsikkapuu-Kaisa","Kiljukaula-Kalle","Riitelevä-Raija","Topakka-Tuula","Tähtitieteellinen-Timo"
         };
 
         public Enemies()
