d89dc44f5bbd50140e584686a90000126c1a8475
xirtoth <henry.heino11@gmail.com>
2023-04-29 17:43:00 +0300

    äänitestailua

--- Item.cs
+++ Item.cs
@@ -6,20 +6,22 @@ namespace School_Project
     public class Item : Entity
     {
         public Action UseAction { get; set; }
+        public SoundType SoundType { get; set; }
 
         public Item(string name, string description, Position pos, char mark, ConsoleColor color) : base(name, description, pos, mark, color)
         {
         }
 
-        public Item(string name, string description, Position pos, char mark, ConsoleColor color, Action useAction) : base(name, description, pos, mark, color)
+        public Item(string name, string description, Position pos, char mark, ConsoleColor color, SoundType soundType, Action useAction) : base(name, description, pos, mark, color)
         {
+            SoundType = soundType;
             UseAction = useAction;
         }
 
         public override async void Use()
         {
             UseAction.Invoke();
-            SoundManager.PlayItemPickupSoundAsync().Wait(1);
+            SoundManager.PlayAsync(SoundType).Wait(1);
             await Task.Delay(TimeSpan.FromSeconds(1));
             SoundManager.PlayMainMusic();
         }
--- Items.cs
+++ Items.cs
@@ -15,34 +15,34 @@ namespace School_Project
         {
             ItemsList = new List<Item>();
 
-            ItemsList.Add(new Item("Koskenkorvapullo", "(+Vahinko -Osumatarkkuus)", new Position(0, 0), '!', ConsoleColor.Blue, () =>
+            ItemsList.Add(new Item("Koskenkorvapullo", "(+Vahinko -Osumatarkkuus)", new Position(0, 0), '!', ConsoleColor.Blue, SoundType.ItemPickup, () =>
             {
                 gc.Player.BaseDamage += 20;
                 gc.Player.BaseHitChance += 10;
                 gc.MessageLog.AddMessage(new LogMessage("Juot pullon, tunnet itsesi voimakkaammaksi, samalla myös osumatarkkuus heikentyy", ConsoleColor.Blue));
             }));
-            ItemsList.Add(new Item("Välivesi", "(+Osumatarkkuus)", new Position(0, 0), '!', ConsoleColor.Cyan, () =>
+            ItemsList.Add(new Item("Välivesi", "(+Osumatarkkuus)", new Position(0, 0), '!', ConsoleColor.Cyan, SoundType.ItemPickup, () =>
             {
                 gc.Player.BaseHitChance -= 15;
                 gc.MessageLog.AddMessage(new LogMessage("Hörppäät väliveden ja maailma näyttää selkeämmältä. Osumatarkkuutesi paranee", ConsoleColor.Blue));
             }));
-            ItemsList.Add(new Item("Vissy", "ilman kossua (+Hp +MaxHp)", new Position(0, 0), '!', ConsoleColor.DarkBlue, () =>
+            ItemsList.Add(new Item("Vissy", "ilman kossua (+Hp +MaxHp)", new Position(0, 0), '!', ConsoleColor.DarkBlue, SoundType.ItemPickup, () =>
             {
                 gc.Player.HitPoints += 100;
                 gc.Player.MaxHp += 50;
                 gc.MessageLog.AddMessage(new LogMessage("Kulautat vissyn naamariin, tunnet voivasi paremmin.", ConsoleColor.Blue));
             }));
-            ItemsList.Add(new Item("Konjakki", "(+Kokemuspisteet)", new Position(0, 0), '!', ConsoleColor.DarkRed, () =>
+            ItemsList.Add(new Item("Konjakki", "(+Kokemuspisteet)", new Position(0, 0), '!', ConsoleColor.DarkRed, SoundType.ItemPickup, () =>
             {
                 gc.Player.AddExperience(100);
                 gc.MessageLog.AddMessage(new LogMessage("Nautiskelet konjakin. Tunnet itsesi ammattilaiseksi", ConsoleColor.Blue));
             }));
-            ItemsList.Add(new Item("Smurffilimu", "(+Hp)", new Position(0, 0), '!', ConsoleColor.DarkGreen, () =>
+            ItemsList.Add(new Item("Smurffilimu", "(+Hp)", new Position(0, 0), '!', ConsoleColor.DarkGreen, SoundType.ItemPickup, () =>
             {
                 gc.Player.HitPoints += 100;
                 gc.MessageLog.AddMessage(new LogMessage("Juot smurffilimun ja tunnet voivasi paremmin", ConsoleColor.Blue));
             }));
-            ItemsList.Add(new Item("Maitotölkki", "(+raha)", new Position(0, 0), '!', ConsoleColor.White, () =>
+            ItemsList.Add(new Item("Maitotölkki", "(+raha)", new Position(0, 0), '!', ConsoleColor.White, SoundType.Fail, () =>
             {
                 gc.Player.Money += 5;
                 gc.MessageLog.AddMessage(new LogMessage("Juot maitotölkin. Mitä ihmettä, sen pohjalta löytyi 5 euron kolikkoa!", ConsoleColor.Blue));
@@ -52,7 +52,7 @@ namespace School_Project
         public Item GetRandomItem()
         {
             var randomItem = ItemsList[random.Next(0, ItemsList.Count())];
-            Item newItem = new(randomItem.Name, randomItem.Description, randomItem.Pos, randomItem.Mark, randomItem.Color, randomItem.UseAction);
+            Item newItem = new(randomItem.Name, randomItem.Description, randomItem.Pos, randomItem.Mark, randomItem.Color, randomItem.SoundType, randomItem.UseAction);
             return newItem;
             //  return ItemsList[random.Next(0, ItemsList.Count())];
         }
